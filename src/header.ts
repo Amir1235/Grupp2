3bf1aeaf29832a48befe7c1fa7d833a3d8e5d21d
gutamuw <129604444+gutamuw@users.noreply.github.com>
2024-12-23 11:32:14 +0100

    Lagt till enkel style och en sökruta. Endast för tydlighet.

--- src/header.ts
+++ src/header.ts
@@ -1,16 +1,16 @@
 const header = document.getElementById("header");
 if(header){
     header.innerHTML = `
-      <div class="logo">Logo</div>
+      <div class="logo"><a href="index.html">Logo</a></div>
+      <div class="search-container"><input type="search"><button><i class="fa fa-search"></i></button></div>
       <nav>
         <ul>
-          <a href="">Hem</a>
-        </ul>
-        <ul>
-          <a href="">Om oss</a>
-        </ul>
-        <ul>
-          <a href="">Kassa</a>
+        <li>
+          <a href="index.html">Hem</a>
+        </li>
+        <li>
+          <a href="checkout.html">Varukorg <i class="fa fa-shopping-cart"></i></a>
+        </li>
         </ul>
       </nav>
     `
