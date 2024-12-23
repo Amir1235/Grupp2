783c3eacbfeff983cc49e5c1c5363e5a15096b44
gutamuw <129604444+gutamuw@users.noreply.github.com>
2024-12-23 10:51:46 +0100

    Lade till header.js

---
+++ src/header.ts
@@ -0,0 +1,17 @@
+const header = document.getElementById("header");
+if(header){
+    header.innerHTML = `
+      <div class="logo">Logo</div>
+      <nav>
+        <ul>
+          <a href="">Hem</a>
+        </ul>
+        <ul>
+          <a href="">Om oss</a>
+        </ul>
+        <ul>
+          <a href="">Kassa</a>
+        </ul>
+      </nav>
+    `
+}
