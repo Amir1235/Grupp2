d35e06ffc1bf5ac949d417de31dcae2249f256be
gutamuw <luddeelv@hotmail.com>
2024-12-26 23:30:32 +0100

    Book-objekten visas på startsidan

--- src/header.ts
+++ src/header.ts
@@ -1,7 +1,7 @@
 const header = document.getElementById("header");
 if(header){
     header.innerHTML = `
-      <div class="logo"><a href="index.html">Logo</a></div>
+      <div class="logo"><a href="index.html">Bokshoppen📚</a></div>
       <div class="search-container"><input type="search"><button><i class="fa fa-search"></i></button></div>
       <nav>
         <ul>
@@ -9,7 +9,7 @@ if(header){
           <a href="index.html">Hem</a>
         </li>
         <li>
-          <a href="checkout.html">Varukorg <i class="fa fa-shopping-cart"></i></a>
+          <a href="checkout.html"><i class="fa fa-shopping-cart"></i></a>
         </li>
         </ul>
       </nav>
---
+++ src/models/Book.ts
@@ -0,0 +1,3 @@
+export class Book {
+    constructor(public imgUrl: string, public name: string, public price: number, public author: string, public genre: string, public isInStock: boolean) {}
+} 
