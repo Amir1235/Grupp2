ee7cfe5f79231b351aead93618ab90e1b85dee29
amircoll1235 <amir_abukar@outlook.com>
2024-12-27 10:01:34 +0100

    produktinfo

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { Book } from './models/Book';
+
 import './style.css'
 
 interface IBook {
+    id:number;
     imgUrl: string;
     name: string;
     price: number; 
@@ -10,13 +12,13 @@ interface IBook {
     isInStock: boolean; 
 }
 
-const book1 = new Book("https://m.media-amazon.com/images/I/81q77Q39nEL._SY522_.jpg", "Harry Potter Book Set", 500, "J.K Rowling", "Fantasy", true);
-const book2 = new Book("https://image.bokus.com/images/9789189585973_383x_omgiven-av-idioter-hur-man-forstar-dem-som-inte-gar-att-forsta_pocket", "Omgiven av idioter", 299, "Thomas Eriksson", "Psykologi", true);
-const book3 = new Book("https://image.bokus.com/images/9789155268282_383x_vargbroder_kartonnage", "Vargbröder", 199, "Michelle Paver", "Barn", true);
-const book4 = new Book("https://m.media-amazon.com/images/I/91EJQU2B4cL._SY522_.jpg", "Eragon", 300, "Christopher Paolini", "Fantasy", false)
+const book1 = new Book(1, "https://m.media-amazon.com/images/I/81q77Q39nEL._SY522_.jpg", "Harry Potter Book Set", 500, "J.K Rowling", "Fantasy", true);
+const book2 = new Book(2, "https://image.bokus.com/images/9789189585973_383x_omgiven-av-idioter-hur-man-forstar-dem-som-inte-gar-att-forsta_pocket", "Omgiven av idioter", 299, "Thomas Eriksson", "Psykologi", true);
+const book3 = new Book(3, "https://image.bokus.com/images/9789155268282_383x_vargbroder_kartonnage", "Vargbröder", 199, "Michelle Paver", "Barn", true);
+const book4 = new Book(4, "https://m.media-amazon.com/images/I/91EJQU2B4cL._SY522_.jpg", "Eragon", 300, "Christopher Paolini", "Fantasy", false);
 
 //Skapar en lista och lägger in Böckerna som vi skapat
-const products = [book1, book2, book3, book4]
+export const products: IBook[] = [book1, book2, book3, book4];
 
 const createBooks = () => {
     //Skapar container för alla böckerna
@@ -39,7 +41,13 @@ const createBooks = () => {
             <button id="addBtn">Lägg till i varukorg</button>
         </div>
         </div>
-        `
+        `;
+        bookInfo.addEventListener("click", () => {
+            window.location.href = `product.html?id=${book.id}`;
+        });
+
+
+
         //Lägger till bookInfo i booksContainer som vi skapade innan loopen
         booksContainer.appendChild(bookInfo)
     })
---
+++ src/product.ts
@@ -0,0 +1,25 @@
+import { products } from './main'
+
+const params = new URLSearchParams(window.location.search);
+const bookId = Number(params.get('id'));
+
+
+const book = products.find((b) => b.id === bookId);
+
+const app = document.getElementById("app");
+
+if (book && app) {
+    app.innerHTML = `
+    <div class="product">
+        <img src="${book.imgUrl}" alt="${book.name}">
+        <h1>${book.name}</h1>
+        <p>Author: ${book.author}</p>
+        <p>Price: ${book.price} kr</p>
+        <p>Genre: ${book.genre}</p>
+        <p>${book.isInStock ? 'In stock' : 'Out of stock'}</p>
+        <button>Buy Now</button>
+    </div>
+    `;
+} else if (app) {
+    app.innerHTML = `<p>Book not found</p>`;
+}
