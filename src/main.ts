1df5722236576db0b4588fa713d80fea28183447
moses kinuka <[kinuka__@hotmail.com]>
2024-12-28 10:33:19 +0100

    skapade local storage för vår kassa

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,71 @@
-import { Book } from './models/Book';
+import { Book } from "./models/Book";
 
-import './style.css'
+import "./style.css";
 
-interface IBook {
-    id:number;
-    imgUrl: string;
-    name: string;
-    price: number; 
-    author: string;
-    genre: string;
-    isInStock: boolean; 
+export interface IBook {
+  id: number;
+  imgUrl: string;
+  name: string;
+  price: number;
+  author: string;
+  genre: string;
+  isInStock: boolean;
 }
 
-const book1 = new Book(1, "https://m.media-amazon.com/images/I/81q77Q39nEL._SY522_.jpg", "Harry Potter Book Set", 500, "J.K Rowling", "Fantasy", true);
-const book2 = new Book(2, "https://image.bokus.com/images/9789189585973_383x_omgiven-av-idioter-hur-man-forstar-dem-som-inte-gar-att-forsta_pocket", "Omgiven av idioter", 299, "Thomas Eriksson", "Psykologi", true);
-const book3 = new Book(3, "https://image.bokus.com/images/9789155268282_383x_vargbroder_kartonnage", "Vargbröder", 199, "Michelle Paver", "Barn", true);
-const book4 = new Book(4, "https://m.media-amazon.com/images/I/91EJQU2B4cL._SY522_.jpg", "Eragon", 300, "Christopher Paolini", "Fantasy", false);
+const book1 = new Book(
+  1,
+  "https://m.media-amazon.com/images/I/81q77Q39nEL._SY522_.jpg",
+  "Harry Potter Book Set",
+  500,
+  "J.K Rowling",
+  "Fantasy",
+  true
+);
+const book2 = new Book(
+  2,
+  "https://image.bokus.com/images/9789189585973_383x_omgiven-av-idioter-hur-man-forstar-dem-som-inte-gar-att-forsta_pocket",
+  "Omgiven av idioter",
+  299,
+  "Thomas Eriksson",
+  "Psykologi",
+  true
+);
+const book3 = new Book(
+  3,
+  "https://image.bokus.com/images/9789155268282_383x_vargbroder_kartonnage",
+  "Vargbröder",
+  199,
+  "Michelle Paver",
+  "Barn",
+  true
+);
+const book4 = new Book(
+  4,
+  "https://m.media-amazon.com/images/I/91EJQU2B4cL._SY522_.jpg",
+  "Eragon",
+  300,
+  "Christopher Paolini",
+  "Fantasy",
+  false
+);
 
 //Skapar en lista och lägger in Böckerna som vi skapat
 export const products: IBook[] = [book1, book2, book3, book4];
 
-const createBooks = () => {
-    //Skapar container för alla böckerna
-    const booksContainer = document.createElement("div")
-    booksContainer.className = "books-container"
+const cartItems = localStorage.getItem("cartItems");
+const storedItems = cartItems || "[]";
+const myBooks: IBook[] = JSON.parse(storedItems);
 
-    //Loopar genom listan products
-    products.forEach((book) => {
-        //Skapar en div (bookInfo) och sätter dess innerHTML till den html-struktur man vill ha - funkar precis som html
-        const bookInfo = document.createElement("div")
-        bookInfo.innerHTML = `
+const createBooks = () => {
+  //Skapar container för alla böckerna
+  const booksContainer = document.createElement("div");
+  booksContainer.className = "books-container";
+
+  //Loopar genom listan products
+  products.forEach((book) => {
+    //Skapar en div (bookInfo) och sätter dess innerHTML till den html-struktur man vill ha - funkar precis som html
+    const bookInfo = document.createElement("div");
+    bookInfo.innerHTML = `
         <div class="product-card">
         <img src="${book.imgUrl}">
         <div class="book-info">
@@ -37,22 +73,30 @@ const createBooks = () => {
             <p>${book.author}</p>
             <p>${book.price} kr</p>
             <p>${book.genre}</p>
-            <p>${book.isInStock ? 'I lager' : 'Ej i lager'}</p> 
-            <button id="addBtn">Lägg till i varukorg</button>
+            <p>${book.isInStock ? "I lager" : "Ej i lager"}</p> 
+            <button class="addBtn" data-id="${
+              book.id
+            }">Lägg till i varukorg</button>
         </div>
         </div>
         `;
-        bookInfo.addEventListener("click", () => {
-            window.location.href = `product.html?id=${book.id}`;
-        });
+    bookInfo.addEventListener("click", () => {
+      window.location.href = `product.html?id=${book.id}`;
+    });
 
+    const addButton = bookInfo.querySelector(".addBtn");
+    addButton?.addEventListener("click", () => {
+      myBooks.push(book);
+      const myStringArray = JSON.stringify(myBooks);
+      localStorage.setItem("cartItems", myStringArray);
+      console.log(myBooks);
+    });
 
-
-        //Lägger till bookInfo i booksContainer som vi skapade innan loopen
-        booksContainer.appendChild(bookInfo)
-    })
-    //Lägger till booksContainer i app
-    document.getElementById("app")?.appendChild(booksContainer)
-}
+    //Lägger till bookInfo i booksContainer som vi skapade innan loopen
+    booksContainer.appendChild(bookInfo);
+  });
+  //Lägger till booksContainer i app
+  document.getElementById("app")?.appendChild(booksContainer);
+};
 //Anropar funktionen så att den körs
-createBooks()
+createBooks();
