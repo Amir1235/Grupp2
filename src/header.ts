9925b3bf33b7a5c4f760fe1e28c78f7fb54d487e
moses kinuka <[kinuka__@hotmail.com]>
2025-01-02 15:04:44 +0100

    la till en funktion för att få totalpris i kassan

--- src/header.ts
+++ src/header.ts
@@ -1,9 +1,22 @@
-
 import { Book } from "./models/Book";
+import { myBooks } from "./main";
+
+export const updatePrice = () => {
+  let totalPrice: number = 0;
+  for (let i = 0; i < myBooks.length; i++) {
+    totalPrice += myBooks[i].price;
+  }
+
+  const totalPriceElement = document.getElementById("totalPrice");
+  if (totalPriceElement) {
+    totalPriceElement.innerHTML = `${totalPrice}kr`;
+  }
+  return totalPrice;
+};
 
 const header = document.getElementById("header");
-if(header){
-    header.innerHTML = `
+if (header) {
+  header.innerHTML = `
     <div class="header-left">
       <div class="logo"><a href="index.html">Bokshoppen📚</a></div>
       <nav>
@@ -27,50 +40,51 @@ if(header){
       <div class="search-container"><input type="search"><button><i class="fa fa-search"></i></button></div>
       <a href="#" class="shoppingCart"><i class="fa fa-shopping-cart"></i></a>
       <div>
-    `
-//Skapar sidebar    
-const sidebar = document.createElement("div");
-sidebar.id = "cart-sidebar";
-sidebar.className = "cart-sidebar hidden";
-sidebar.innerHTML = `
+    `;
+  //Skapar sidebar
+  const sidebar = document.createElement("div");
+  sidebar.id = "cart-sidebar";
+  sidebar.className = "cart-sidebar hidden";
+  sidebar.innerHTML = `
   <button id="close-cart-sidebar" class="close-btn"><i class="fa fa-close"></i></button>
   <h2>Din varukorg</h2>
   <div id="cart-items-container"></div>
+  <h2>Totalpris inkl. moms:</h2>
+  <p id="totalPrice"></p>
   <a href="checkout.html"><button class="to-cart-btn">Till kassa</button></a>
 `;
-document.body.appendChild(sidebar);
+  document.body.appendChild(sidebar);
 
-//Eventlyssnare
-const cartButton = document.querySelector(".shoppingCart");
-cartButton?.addEventListener("click", () => {
-  const sidebar = document.getElementById("cart-sidebar");
-  const cartItemsContainer = document.getElementById("cart-items-container");
+  //Eventlyssnare
+  const cartButton = document.querySelector(".shoppingCart");
+  cartButton?.addEventListener("click", () => {
+    const sidebar = document.getElementById("cart-sidebar");
+    const cartItemsContainer = document.getElementById("cart-items-container");
 
-  if (sidebar && cartItemsContainer) {
-    sidebar.classList.add("open");
+    if (sidebar && cartItemsContainer) {
+      sidebar.classList.add("open");
 
-    const storedItems = localStorage.getItem("cartItems") || "[]";
-    const myBooks = JSON.parse(storedItems);
+      const storedItems = localStorage.getItem("cartItems") || "[]";
+      const myBooks = JSON.parse(storedItems);
 
-    cartItemsContainer.innerHTML = "";
-    if (myBooks.length) {
-      myBooks.forEach((book:Book) => {
-        const productInCart = document.createElement("div");
-        productInCart.className = "product-in-cart"
-        productInCart.innerHTML = `<img src="${book.imgUrl}"> <div><p>${book.name}</p> <p>${book.author}</p></div>`;
-        cartItemsContainer.appendChild(productInCart);
-      });
-    } else {
-      cartItemsContainer.innerHTML = "<li>Din varukorg är tom</li>";
+      cartItemsContainer.innerHTML = "";
+      if (myBooks.length) {
+        myBooks.forEach((book: Book) => {
+          const productInCart = document.createElement("div");
+          productInCart.className = "product-in-cart";
+          productInCart.innerHTML = `<img src="${book.imgUrl}"> <div><p>${book.name}</p> <p>${book.author}</p> <p>${book.price}kr</p> </div>`;
+          cartItemsContainer.appendChild(productInCart);
+        });
+      } else {
+        cartItemsContainer.innerHTML = "<li>Din varukorg är tom</li>";
+      }
+      updatePrice();
     }
-  }
-});
+  });
 
-
-const closeButton = document.getElementById("close-cart-sidebar");
-closeButton?.addEventListener("click", () => {
-  const sidebar = document.getElementById("cart-sidebar");
-  sidebar?.classList.remove("open");
-});
-
+  const closeButton = document.getElementById("close-cart-sidebar");
+  closeButton?.addEventListener("click", () => {
+    const sidebar = document.getElementById("cart-sidebar");
+    sidebar?.classList.remove("open");
+  });
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { Book } from "./models/Book";
-
+import { updatePrice } from "./header";
 import "./style.css";
 
 export interface IBook {
@@ -21,7 +21,9 @@ const book1 = new Book(
   500,
   "J.K Rowling",
   "Fantasy",
-  true,"När ett brev kommer till den olyckliga men vanliga Harry Potter, avslöjas en decennium gammal hemlighet för honom som uppenbarligen är den sista att veta. Hans föräldrar var trollkarlar, dödade av en mörkerherres förbannelse när Harry bara var en bebis, och som han på något sätt överlevde. Harry lämnar sin osympatiska moster och farbror till Hogwarts, en trollkarlsskola full av spöken och förtrollningar, och snubblar över ett olycksbådande mysterium när han hittar en trehövdad hund som vaktar ett rum på tredje våningen. Sedan får han höra om en saknad sten med häpnadsväckande krafter som kan vara värdefull, farlig - eller både och. Ett otroligt äventyr är på väg att börja!","⭐⭐⭐⭐ 70 Recensioner"
+  true,
+  "När ett brev kommer till den olyckliga men vanliga Harry Potter, avslöjas en decennium gammal hemlighet för honom som uppenbarligen är den sista att veta. Hans föräldrar var trollkarlar, dödade av en mörkerherres förbannelse när Harry bara var en bebis, och som han på något sätt överlevde. Harry lämnar sin osympatiska moster och farbror till Hogwarts, en trollkarlsskola full av spöken och förtrollningar, och snubblar över ett olycksbådande mysterium när han hittar en trehövdad hund som vaktar ett rum på tredje våningen. Sedan får han höra om en saknad sten med häpnadsväckande krafter som kan vara värdefull, farlig - eller både och. Ett otroligt äventyr är på väg att börja!",
+  "⭐⭐⭐⭐ 70 Recensioner"
 );
 const book2 = new Book(
   2,
@@ -30,7 +32,9 @@ const book2 = new Book(
   299,
   "Thomas Eriksson",
   "Psykologi",
-  true,"Omgiven av idioter beskriver konkret och underhållande en av världens mest spridda metoder för att sortera olikheterna inom mänsklig kommunikation. Med vetenskapliga grund och genom vardagens möten ger den dig konkreta hjälpmedel att förstå de viktigaste skillnaderna mellan olika kommunikationsstilar. Den visar på en av de vanligaste orsakerna till konflikter i vardagen: dålig kommunikation. Och den ger dig som läsare svaret på frågan: Vad gör jag åt det?","⭐⭐⭐ 28 Recensioner"
+  true,
+  "Omgiven av idioter beskriver konkret och underhållande en av världens mest spridda metoder för att sortera olikheterna inom mänsklig kommunikation. Med vetenskapliga grund och genom vardagens möten ger den dig konkreta hjälpmedel att förstå de viktigaste skillnaderna mellan olika kommunikationsstilar. Den visar på en av de vanligaste orsakerna till konflikter i vardagen: dålig kommunikation. Och den ger dig som läsare svaret på frågan: Vad gör jag åt det?",
+  "⭐⭐⭐ 28 Recensioner"
 );
 const book3 = new Book(
   3,
@@ -39,7 +43,9 @@ const book3 = new Book(
   199,
   "Michelle Paver",
   "Barn",
-  true,"Toraks far är döende och han själv är skadad efter den stora demonbjörnens attack. Torak blir tvungen att svära på sin fars dödsbädd att han skall söka upp Världsandens berg och befria Storskogen från demonen och dess skapare. En kort tid efter att han flytt från sitt och sin fars läger möter han en vargunge och upptäcker att han kan vargarnas språk. Vargungens föräldrar hade blivit dödade av en översvämning och Torak tar sig an ungen och döper honom till Ulv. Senare, när Torak fortsätter sin färd mot världsandens berg, blir han tillfångatagen av Korpklanen som anklagar honom för att ha stulit deras byte och han blir förd till deras läger.","⭐⭐⭐⭐⭐ 81 Recensioner"
+  true,
+  "Toraks far är döende och han själv är skadad efter den stora demonbjörnens attack. Torak blir tvungen att svära på sin fars dödsbädd att han skall söka upp Världsandens berg och befria Storskogen från demonen och dess skapare. En kort tid efter att han flytt från sitt och sin fars läger möter han en vargunge och upptäcker att han kan vargarnas språk. Vargungens föräldrar hade blivit dödade av en översvämning och Torak tar sig an ungen och döper honom till Ulv. Senare, när Torak fortsätter sin färd mot världsandens berg, blir han tillfångatagen av Korpklanen som anklagar honom för att ha stulit deras byte och han blir förd till deras läger.",
+  "⭐⭐⭐⭐⭐ 81 Recensioner"
 );
 const book4 = new Book(
   4,
@@ -48,7 +54,9 @@ const book4 = new Book(
   300,
   "Christopher Paolini",
   "Fantasy",
-  false,"När Eragon hittar en slipad blå sten i skogen tror han att det är den lyckliga upptäckten av en fattig bondpojke; kanske kommer det att köpa hans familj kött för vintern. Men när stenen ger en drake som kläcks, inser Eragon snart att han har snubblat över ett arv som är nästan lika gammalt som själva imperiet.","⭐⭐⭐⭐⭐ 35 Recensioner"
+  false,
+  "När Eragon hittar en slipad blå sten i skogen tror han att det är den lyckliga upptäckten av en fattig bondpojke; kanske kommer det att köpa hans familj kött för vintern. Men när stenen ger en drake som kläcks, inser Eragon snart att han har snubblat över ett arv som är nästan lika gammalt som själva imperiet.",
+  "⭐⭐⭐⭐⭐ 35 Recensioner"
 );
 const book5 = new Book(
   5,
@@ -57,7 +65,9 @@ const book5 = new Book(
   300,
   "Jane  Austen",
   "Romantik",
-  false,"Pride and Prejudice följer det turbulenta förhållandet mellan Elizabeth Bennet, dotter till en herre på landet, och Fitzwilliam Darcy, en rik aristokratisk godsägare. De måste övervinna de titulära synderna av stolthet och fördomar för att bli kära och gifta sig.","⭐⭐⭐ 17 Recensioner"
+  false,
+  "Pride and Prejudice följer det turbulenta förhållandet mellan Elizabeth Bennet, dotter till en herre på landet, och Fitzwilliam Darcy, en rik aristokratisk godsägare. De måste övervinna de titulära synderna av stolthet och fördomar för att bli kära och gifta sig.",
+  "⭐⭐⭐ 17 Recensioner"
 );
 const book6 = new Book(
   6,
@@ -66,7 +76,9 @@ const book6 = new Book(
   350,
   "Mary Shelley",
   "Skräck",
-  true,"Avskuren från sin familj och ägnade sig åt sitt arbete vid Ingolstadt University, lärde Victor Frankenstein hemligheten med att skapa liv. Efter att ha plundrat kyrkogårdar för mänskliga lemmar och organ, bygger han en varelse och väcker den till liv. När han ser resultatet flyr Frankenstein från sin skapelse i skräck","⭐⭐⭐ 24 Recensioner"
+  true,
+  "Avskuren från sin familj och ägnade sig åt sitt arbete vid Ingolstadt University, lärde Victor Frankenstein hemligheten med att skapa liv. Efter att ha plundrat kyrkogårdar för mänskliga lemmar och organ, bygger han en varelse och väcker den till liv. När han ser resultatet flyr Frankenstein från sin skapelse i skräck",
+  "⭐⭐⭐ 24 Recensioner"
 );
 const book7 = new Book(
   7,
@@ -75,7 +87,9 @@ const book7 = new Book(
   250,
   "Astrid Lindgren",
   "Barn",
-  true,"Pippi Långstrump är en nioårig flicka som en dag ensam flyttar in i Villa Villekulla i den lilla staden. Med sig har hon sin apa, Herr Nilsson, och sin häst, Lilla Gubben. Dessutom har hon en kappsäck full med gullpengar. Pippilotta Viktualia Rullgardina Krusmynta Efraimsdotter Långstrump är en märkvärdig flicka.","⭐⭐⭐⭐⭐ 67 Recensioner"
+  true,
+  "Pippi Långstrump är en nioårig flicka som en dag ensam flyttar in i Villa Villekulla i den lilla staden. Med sig har hon sin apa, Herr Nilsson, och sin häst, Lilla Gubben. Dessutom har hon en kappsäck full med gullpengar. Pippilotta Viktualia Rullgardina Krusmynta Efraimsdotter Långstrump är en märkvärdig flicka.",
+  "⭐⭐⭐⭐⭐ 67 Recensioner"
 );
 const book8 = new Book(
   8,
@@ -84,7 +98,9 @@ const book8 = new Book(
   245,
   "Camilla Läckberg, Henrik Fexeus",
   "Deckare",
-  false,"Det är december i Stockholm och Sveriges justitieminister befinner sig under hot. I samma veva hittas en hög människoben nere i Stockholms tunnelbana, och skelettet visar sig komma från en högt uppsatt finansman. Polisinspektör Mina Dabiris grupp är stukad efter den traumatiska händelsen i somras som slutade i en kollegas död, och kallar in mentalisten Vincent Walder för att hjälpa till med fallet. För Vincent känns det som om världen alltmer sluter sig kring honom. När ytterligare en benhög hittas i underjorden sätts gruppen återigen på prov – vad är det som pågår i tunnlarna djupt under Stockholm? Och vem är det som jagar ministern?","⭐⭐ 44 Recensioner"
+  false,
+  "Det är december i Stockholm och Sveriges justitieminister befinner sig under hot. I samma veva hittas en hög människoben nere i Stockholms tunnelbana, och skelettet visar sig komma från en högt uppsatt finansman. Polisinspektör Mina Dabiris grupp är stukad efter den traumatiska händelsen i somras som slutade i en kollegas död, och kallar in mentalisten Vincent Walder för att hjälpa till med fallet. För Vincent känns det som om världen alltmer sluter sig kring honom. När ytterligare en benhög hittas i underjorden sätts gruppen återigen på prov – vad är det som pågår i tunnlarna djupt under Stockholm? Och vem är det som jagar ministern?",
+  "⭐⭐ 44 Recensioner"
 );
 const book9 = new Book(
   9,
@@ -93,8 +109,10 @@ const book9 = new Book(
   250,
   "Lynn Painter",
   "Young Adult",
-  true,"Under några vackra månader hade Wes sin drömtjej: den viljestarka grannetjejen Liz. Men precis när de två skulle ge sig av till college för att börja sitt förstaårsår tillsammans, slog tragedin till. Wes fick ta itu med nedfallet, vilket i slutändan innebar att han förlorade Liz i processen, Blinkar framåt månader senare och Wes och Liz befinner sig på college tillsammans. På en hälsosammare plats nu vet Wes att han krossade Liz hjärta när han avslutade saker, men han är fast besluten att få henne att bli kär i honom igen. Wes känner Liz bättre än någon annan, och han har en idiotsäker plan för att vinna tillbaka henne med rom-com-värdiga stora gester hon älskar. Endast . . . Liz kommer inte ha något av det ! Wes måste planera som en rom-com-hjälte för att komma på hur han ska se henne. Ännu värre, Liz har en ny vän. . . en killkompis . Ändå kommer Wes inte att ge upp, anpassa sina smarta planer och anstränga sig för att få Liz uppmärksamhet och vinna tillbaka hennes tillgivenhet. Men efter att hans bästa ansträngningar inte fått honom någonstans, undrar Wes om deras förhållande verkligen är över för gott.","⭐⭐⭐⭐⭐ 69 Recensioner"
-)
+  true,
+  "Under några vackra månader hade Wes sin drömtjej: den viljestarka grannetjejen Liz. Men precis när de två skulle ge sig av till college för att börja sitt förstaårsår tillsammans, slog tragedin till. Wes fick ta itu med nedfallet, vilket i slutändan innebar att han förlorade Liz i processen, Blinkar framåt månader senare och Wes och Liz befinner sig på college tillsammans. På en hälsosammare plats nu vet Wes att han krossade Liz hjärta när han avslutade saker, men han är fast besluten att få henne att bli kär i honom igen. Wes känner Liz bättre än någon annan, och han har en idiotsäker plan för att vinna tillbaka henne med rom-com-värdiga stora gester hon älskar. Endast . . . Liz kommer inte ha något av det ! Wes måste planera som en rom-com-hjälte för att komma på hur han ska se henne. Ännu värre, Liz har en ny vän. . . en killkompis . Ändå kommer Wes inte att ge upp, anpassa sina smarta planer och anstränga sig för att få Liz uppmärksamhet och vinna tillbaka hennes tillgivenhet. Men efter att hans bästa ansträngningar inte fått honom någonstans, undrar Wes om deras förhållande verkligen är över för gott.",
+  "⭐⭐⭐⭐⭐ 69 Recensioner"
+);
 const book10 = new Book(
   10,
   "https://bilder.akademibokhandeln.se/images_akb/9780349439358_383/taming-7",
@@ -102,7 +120,9 @@ const book10 = new Book(
   250,
   "Chloe Walsh",
   "Drama",
-  true, "Hon är den typiska solskenstjejen. Han är den älskvärda klassens clown. Men stormmoln rullar in, och den här Tommen-pojken är på väg att bli allvar. Tommens fräckaste pojke, Gerard 'Gibsie' Gibson, har alltid varit en komiker, men de flesta ser inte vad som döljer sig under ytan. Han hemsöks av händelser från det förflutna och han använder humor för att klara sig och döljer sitt sanna jag från världen. Claire Biggs, symbolen för solsken, har alltid älskat Gibsie, hennes brors vän och hennes favoritgranne. Hon har alltid sett en sida hos honom som ingen annan verkar lägga märke till. Hon blir fast besluten att tämja sin vilda barndomsbästa vän. När gränserna korsas blir det oklart om Gibsie och Claires vänskap kommer att överleva. Kommer det att blomma ut till något mer, eller kommer de att förlora sig själva i det vilda?","⭐⭐⭐⭐ 52 Recensioner"
+  true,
+  "Hon är den typiska solskenstjejen. Han är den älskvärda klassens clown. Men stormmoln rullar in, och den här Tommen-pojken är på väg att bli allvar. Tommens fräckaste pojke, Gerard 'Gibsie' Gibson, har alltid varit en komiker, men de flesta ser inte vad som döljer sig under ytan. Han hemsöks av händelser från det förflutna och han använder humor för att klara sig och döljer sitt sanna jag från världen. Claire Biggs, symbolen för solsken, har alltid älskat Gibsie, hennes brors vän och hennes favoritgranne. Hon har alltid sett en sida hos honom som ingen annan verkar lägga märke till. Hon blir fast besluten att tämja sin vilda barndomsbästa vän. När gränserna korsas blir det oklart om Gibsie och Claires vänskap kommer att överleva. Kommer det att blomma ut till något mer, eller kommer de att förlora sig själva i det vilda?",
+  "⭐⭐⭐⭐ 52 Recensioner"
 );
 
 export const products: Book[] = [
@@ -120,12 +140,13 @@ export const products: Book[] = [
 
 const cartItems = localStorage.getItem("cartItems");
 const storedItems = cartItems || "[]";
-const myBooks: Book[] = JSON.parse(storedItems);
+export const myBooks: Book[] = JSON.parse(storedItems);
+console.log(myBooks);
 let filteredProducts = [...products];
 const booksContainer = document.createElement("div");
 
 const createBooks = () => {
-  booksContainer.innerHTML = ""; 
+  booksContainer.innerHTML = "";
   booksContainer.className = "books-container";
   filteredProducts.forEach((book) => {
     const bookInfo = document.createElement("div");
@@ -136,13 +157,11 @@ const createBooks = () => {
             <h3>${book.name}</h3>
             <p>${book.author}</p>
             <p>${book.price} kr</p>
-            <button class="addBtn" data-id="${
-              book.id
-            }">Lägg till i varukorg</button>
+            <button class="addBtn" data-id="${book.id}">Lägg till i varukorg</button>
         </div>
         </div>
         `;
-        
+
     const bookImage = bookInfo.querySelector(".book-image");
 
     if (bookImage) {
@@ -156,33 +175,30 @@ const createBooks = () => {
       myBooks.push(book);
       const myStringArray = JSON.stringify(myBooks);
       localStorage.setItem("cartItems", myStringArray);
-      console.log(myBooks);
+      updatePrice();
     });
     booksContainer.appendChild(bookInfo);
-    
   });
   document.getElementById("app")?.appendChild(booksContainer);
 };
 createBooks();
 
-
 // Sortering
 const sortButtons = document.querySelectorAll(".sort-buttons button");
-const allBtn = document.getElementById("allBtn")
+const allBtn = document.getElementById("allBtn");
 
-allBtn?.focus()
+allBtn?.focus();
 
 sortButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    
     const genre = button.textContent?.trim(); // Hämtar genren från knappens text.
 
     if (genre === "Alla") {
-      filteredProducts = [...products]; 
+      filteredProducts = [...products];
     } else {
       filteredProducts = products.filter((product) => product.genre === genre);
     }
-    
-    createBooks(); 
+
+    createBooks();
   });
 });
