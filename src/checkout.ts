0d89e1394d9d61677d04dc9d1f493ba879b94e70
Annie Olofsson <Annie.Olofsson@medieinstitutet.se>
2025-01-08 14:08:08 +0100

    styleat checkout, gjort inmatningsfält och leveransalternativ

--- src/checkout.ts
+++ src/checkout.ts
@@ -29,3 +29,81 @@ function displayCart() {
   }
 }
 displayCart();
+
+function personalInfo() {
+  const app = document.getElementById("app");
+
+  //Postnummer
+  const zipContainer = document.createElement("div");
+  zipContainer.id = "zipContainer";
+  zipContainer.innerHTML = `
+    <div class="zipfield">
+    <h1>Leveranssätt</h1>
+    <h2>Postnummer</h2>
+      <input type="text">
+    </div>
+      <div class="delivery-options">
+      <div class="delopt1">
+        <h3>Hemleverans på morgonen<p>39 kr<img src="https://cdn.ingrid.com/static/shipping-logos/logo-early-bird-round-30.svg"></p></h3>
+        <p>Leveras på torsdag den 16 januari</p>
+        <div class="click"></div>
+      </div>
+      <div class="delopt2">        
+        <h3>Leverans till paketskåp<p>39 kr<img src="https://cdn.ingrid.com/static/shipping-logos/logo-instabox-round-30.svg"></p></h3>
+        <p>Leveras på torsdag den 16 januari</p>
+        <div class="click"></div>
+
+      </div>
+      <div class="delopt3">        
+        <h3>Leverans till ombud<p>39 kr<img src="https://cdn.ingrid.com/static/shipping-logos/logo-db-schenker-round-30.svg"></p></h3>
+        <p>Leveras på torsdag den 16 januari</p>
+        <div class="click"></div>
+        </div>
+      </div>
+      <div class="delopt4">        
+        <h3>Hemleverans på kvällen<p>59 kr<img src="https://cdn.ingrid.com/static/shipping-logos/logo-airmee-round-30.svg"></p></h3>             
+        <p>Leveras på torsdag den 16 januari mellan 17:00 - 22:00</p>
+        <div class="click"></div>
+      </div>
+      <div class="delopt5">        
+       <h3>Ekonomifrakt <p>35 kr</p></h3>
+        <p>Leveras på torsdag den 16 januari<p>35 kr</p></p>
+        <div class="click">
+        </div>
+
+      </div>
+
+    </div>
+  `;
+
+  app?.appendChild(zipContainer);
+
+  const click = document.querySelector("click");
+
+
+  click?.addEventListener("click", () => {
+    click.className = "fill";
+  })
+  //Ange personuppgifter
+  const infoContainer = document.createElement("div");
+  infoContainer.id = "infoContainer";
+
+  app?.appendChild(infoContainer);
+    
+  infoContainer.innerHTML = `
+      <h1 class="infoh1">Personuppgifter</h1>
+      <h3 class="infoh3">Namn</h3>
+      <input type="text" placeholder="För- och Efternamn">
+      <h3 class="infoh3">Epostadress</h3>
+      <input type="text" placeholder="Epostadress"> 
+      <h3 class="infoh3">Telefonummer</h3>
+      <input type="text" placeholder="Telefonummer">
+      <br>
+      <button>Slutför köp</button>
+    `;
+
+    //
+    
+}
+
+personalInfo();
